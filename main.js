408c3d5ed578ade335f84bbb507cbe9e1bce592d
Maria Manoela <manoelamvc@hotmail.com>
2021-09-06 16:10:03 -0300

    Add shadow to header when scrolling the page

--- main.js
+++ main.js
@@ -16,3 +16,15 @@ for(const link of links) {
         nav.classList.remove('show')
     })
 }
+
+// add shadow to header when scrolling the page
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+    if(window.scrollY >= navHeight) {
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll')
+    }
+})
