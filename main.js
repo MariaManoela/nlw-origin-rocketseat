5e80f4cadfb576a122c19191fbae997ec9d9f4b0
Maria Manoela <manoelamvc@hotmail.com>
2021-09-22 15:52:35 -0300

    Activate menu according to visible section

--- main.js
+++ main.js
@@ -18,9 +18,10 @@ for(const link of links) {
 }
 
 // Add shadow to header when scrolling the page
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
 function changeHeaderWhenScroll(){
-    const header = document.querySelector('#header')
-    const navHeight = header.offsetHeight
 
     if(window.scrollY >= navHeight) {
         header.classList.add('scroll')
@@ -64,8 +65,9 @@ scrollReveal.reveal(
 )
 
 // Back to top buttton
+const backToTopButton = document.querySelector('.back-to-top')
+
 function backToTop(){
-    const backToTopButton = document.querySelector('.back-to-top')
 
     if(window.scrollY >= 560){
         backToTopButton.classList.add('show')
@@ -74,8 +76,36 @@ function backToTop(){
     }
 }
 
+// Activate menu according to visible section
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection(){
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for(const section of sections){
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if(checkpointStart && checkpointEnd){
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.add('active')
+        }else{
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.remove('active')
+        }
+    }
+}
+
 // When scroll
 window.addEventListener('scroll', function() {
     changeHeaderWhenScroll()
     backToTop()
+    activateMenuAtCurrentSection()
 })
+
